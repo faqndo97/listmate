0e896d1cc5a6c5b914d9c2748a5ee644215ef6a6
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2023-04-06 00:43:34 -0300

    Store user lists in cookies so they can still be accessible

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,9 @@
 class ListsController < ApplicationController
   before_action :set_list, only: %i[show edit update destroy]
+  before_action :refresh_list_ids_in_cookies
 
   def index
-    @lists = List.all
+    @lists = List.where(share_id: list_ids_from_cookies)
   end
 
   def new
@@ -17,6 +18,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
 
     if @list.save
+      add_list_to_user_cookies
       redirect_to @list, notice: "List was successfully created."
     else
       render :new, status: :unprocessable_entity
@@ -39,5 +41,27 @@ class ListsController < ApplicationController
 
   def list_params
     params.require(:list).permit(:title, items_attributes: [:id, :description, :completed, :_destroy])
+  end
+
+  def list_ids_from_cookies
+    return [] unless cookies[:lists]
+
+    JSON.parse(cookies[:lists])
+  end
+
+  def add_list_to_user_cookies
+    list_share_ids = cookies[:lists] ? JSON.parse(cookies[:lists]) : []
+    list_share_ids << @list.share_id
+
+    cookies[:lists] = JSON.generate list_share_ids
+  end
+
+  def refresh_list_ids_in_cookies
+    return unless cookies[:lists]
+
+    list_share_ids = JSON.parse(cookies[:lists])
+    list_share_ids = List.where(share_id: list_share_ids).pluck :share_id
+
+    cookies[:lists] = JSON.generate list_share_ids
   end
 end
